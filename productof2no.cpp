413c7a3fff9b3ff537f3bf67db212d63b8877344
patilsakshi123 <67007239+patilsakshi123@users.noreply.github.com>
2020-07-09 11:17:35 +0530

    Add files via upload

---
+++ productof2no.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,product;
+cout<<"enter two numbers:\n";
+cin>>a>>b;
+product=a*b;
+cout<<"product is :"<<product<<endl;
+return 0;
+}
