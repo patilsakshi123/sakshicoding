a43a16e79db81320c6add6a63f7015d333f4c90f
patilsakshi123 <67007239+patilsakshi123@users.noreply.github.com>
2020-07-09 11:34:04 +0530

    Add files via upload

---
+++ eligiblityof user..cpp
@@ -0,0 +1,20 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"Enter age of a user:";
+	cin>>age;
+	if(age>=18)
+	{
+		cout<<"\n you are eligible";
+		
+	}
+	else
+	{
+		cout<<"\n You are not eligible";
+		
+	}
+	return 0;
+}
